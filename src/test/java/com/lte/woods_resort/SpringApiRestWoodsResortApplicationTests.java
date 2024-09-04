066c09adfca7f6c2ed126323e5f2cb52b1c329c9
Randy <roscal-202379@kinal.edu.gt>
2024-09-04 07:35:32 -0600

    build($version): primera version

---
+++ src/test/java/com/lte/woods_resort/SpringApiRestWoodsResortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lte.woods_resort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiRestWoodsResortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
