957d6d7a7950e4227ee3aaa0904a4be120aea4bf
MoralesR-2020045 <rmorales215327@gmail.com>
2024-09-05 21:33:30 -0600

    build($Services/HotelService):Creacion de la clase service implementando la interfaz con los metodos abstractos

--- src/main/java/com/lte/woods_resort/services/IHotelsService.java
+++ src/main/java/com/lte/woods_resort/services/IHotelsService.java
@@ -1,11 +1,12 @@
 package com.lte.woods_resort.services;
 
 import java.util.List;
-public interface IHotelsService {
-    
-     public List<Hotels> lisHoteles();
-     public Hotels searchHotels();
-     public Hotels saveHotels();
-     public void removeHotels();
 
+import com.lte.woods_resort.models.Hotels;
+
+public interface IHotelsService {
+     public List<Hotels> listHoteles();
+     public Hotels searchHotels(Long idHotel);
+     public Hotels saveHotels(Hotels hotel);
+     public void removeHotels(Hotels hotel);
 }
