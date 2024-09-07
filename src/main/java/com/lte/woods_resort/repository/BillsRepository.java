f27c4dbc5d464fdc7b5c9c72f5966381dd5d6f96
Zodiacal0 <javierherrera5513@gmail.com>
2024-09-07 13:52:55 -0600

    .

--- src/main/java/com/lte/woods_resort/repository/BillsRepository.java
+++ src/main/java/com/lte/woods_resort/repository/BillsRepository.java
@@ -6,4 +6,5 @@ import com.lte.woods_resort.models.Bills;
 
 public interface BillsRepository extends JpaRepository <Bills, Long> {
 
+    
 }
--- src/main/java/com/lte/woods_resort/services/BillsService.java
+++ src/main/java/com/lte/woods_resort/services/BillsService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.lte.woods_resort.models.Bills;
 import com.lte.woods_resort.repository.BillsRepository;
 
+
 @Service
 public class BillsService implements IBillsService {
 
