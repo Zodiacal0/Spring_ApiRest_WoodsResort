b568f17228216b58301b29d54da5b13591421bd2
Zodiacal0 <javierherrera5513@gmail.com>
2024-09-07 15:24:32 -0600

    feat($service/roomsservice): implementación del servicio de room

---
+++ src/main/java/com/lte/woods_resort/services/RoomsService.java
@@ -0,0 +1,42 @@
+package com.lte.woods_resort.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lte.woods_resort.exceptions.RoomsException;
+import com.lte.woods_resort.models.Rooms;
+import com.lte.woods_resort.repository.RoomsRespository;
+
+@Service
+public class RoomsService implements IRoomsService {
+
+    @Autowired
+    private RoomsRespository roomsRespository;
+
+    @Override
+    public List<Rooms> listRooms() {
+        return roomsRespository.findAll();
+    }
+
+    @Override
+    public Rooms findRooms(String id_rooms) {
+        Rooms rooms = roomsRespository.findById(id_rooms).orElse(null);
+        return rooms;
+    }
+
+    @Override
+    public Rooms saveRooms(Rooms rooms) {
+        if (roomsRespository.existsById(rooms.getId_rooms())) {
+            throw new RoomsException("this rooms already exist");
+        }
+        return roomsRespository.save(rooms);
+    }
+
+    @Override
+    public void deleteRooms(Rooms rooms) {
+        roomsRespository.delete(rooms);
+    }
+
+}
