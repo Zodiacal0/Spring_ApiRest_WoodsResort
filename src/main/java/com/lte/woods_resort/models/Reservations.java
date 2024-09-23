6a6c91a5d2e98a6ee44a27adb644392fe4ad6170
Zodiacal0 <javierherrera5513@gmail.com>
2024-09-23 15:43:46 -0600

    fix(model): etiquetas

--- src/main/java/com/lte/woods_resort/models/Reservations.java
+++ src/main/java/com/lte/woods_resort/models/Reservations.java
@@ -2,6 +2,10 @@ package com.lte.woods_resort.models;
 
 import java.io.Serializable;
 import java.sql.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -10,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -32,5 +37,10 @@ public class Reservations implements Serializable {
     @JoinColumn(name = "id_user", referencedColumnName = "id_user")
     @ManyToOne
     private Users id_user;
+
+    @OneToMany(mappedBy = "reservation")
+    @JsonIgnore
+    @JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+    private List<Rooms> rooms;
 
 }
